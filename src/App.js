d381036d621a595e5354b6cb33e39943ac2c1d0b
Lâm Minh Thiện - 60136962 <lamminhthien02012000@gmail.com>
2022-02-12 11:32:46 +0700

    Creating the Card Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Home from './components/pages/Home'
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/pages/Home"
+import Services from './components/pages/Services'
+import Product from "./components/pages/Product"
+import SignUp from "./components/pages/SignUp"
 
 function App() {
   return (
@@ -10,7 +13,10 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact component={Home}  />
+          <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={Product} />
+          <Route path="/sign-up" component={SignUp} />
         </Switch>
       </Router>
     </>
